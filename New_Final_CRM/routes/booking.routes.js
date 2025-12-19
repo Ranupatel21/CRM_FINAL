d624ae4a2a7d4001b6b61c2427422c17a36b4399
Ranu  Patel <ranup8174@gmail.com>
2025-12-19 13:09:01 +0000

    update new schema car , car routes

--- New_Final_CRM/routes/booking.routes.js
+++ New_Final_CRM/routes/booking.routes.js
@@ -1,32 +1,32 @@
 import express from "express";
 import { Booking } from "../models/booking.model.js";
-import {Cars} from "../models/car.model.js"
+import {Car} from "../models/car.model.js"
 const router = express.Router();
 
 // Brand dropdown
 router.get("/cars/brands", async (req, res) => {
-  const brands = await Cars.distinct("brand");
+  const brands = await Car.distinct("brand");
   res.json(brands);
 });
 
 // Model dropdown
 router.get("/cars/models", async (req, res) => {
   const { brand } = req.query;
-  const models = await Cars.distinct("model", { brand });
+  const models = await Car.distinct("model", { brand });
   res.json(models);
 });
 
 // Variant dropdown
 router.get("/cars/variants", async (req, res) => {
   const { brand, model } = req.query;
-  const variants = await Cars.distinct("variant", { brand, model });
+  const variants = await Car.distinct("variant", { brand, model });
   res.json(variants);
 });
 
 // Color dropdown
 router.get("/cars/colors", async (req, res) => {
   const { brand, model, variant } = req.query;
-  const colors = await Cars.distinct("color", {
+  const colors = await Car.distinct("color", {
     brand,
     model,
     variant
