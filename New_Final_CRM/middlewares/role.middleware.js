4f46e3d8a22b8880c620a27636d8f5814bdc24b4
Ranu  Patel <ranup8174@gmail.com>
2025-12-19 10:22:20 +0000

    update middlewres

--- New_Final_CRM/middlewares/role.middleware.js
+++ New_Final_CRM/middlewares/role.middleware.js
@@ -1,13 +1,15 @@
 console.log("ROLE MIDDLEWARE LOADED");
 
- export const isAdmin = (req, res, next) => {
-  if (!req.user) {
-    return res.status(401).json({ message: "User missing" });
-  }
+  export const allowRoles = (...roles) => {
+  return (req, res, next) => {
+    if (!req.user) {
+      return res.status(401).json({ message: "User missing" });
+    }
 
-  if (req.user.role !== "Admin") {
-    return res.status(403).json({ message: "Access denied" });
-  }
+    if (!roles.includes(req.user.role)) {
+      return res.status(403).json({ message: "Access denied" });
+    }
 
-  next();
+    next();
+  };
 };
