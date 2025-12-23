c8d597dc5c0ecc92b23af449f43d6637899026fa
Ranu  Patel <ranup8174@gmail.com>
2025-12-23 12:38:38 +0000

    update inventory routes, inventory routes

--- New_Final_CRM/routes/inventory.routes.js
+++ New_Final_CRM/routes/inventory.routes.js
@@ -1,49 +1,73 @@
 import express from "express";
 import { Inventory } from "../models/inventory.model.js";
+import multer from "multer";
+import xlsx from "xlsx";
+
 
 const router = express.Router();
 
+const upload = multer({ storage: multer.memoryStorage() });
 
-router.post("/inventory", async (req, res) => {
+router.post("/inventory", upload.single("file"), async (req, res) => {
   try {
-    const { brand, model, variant, color, quantity, price } = req.body;
-
-    const inventory = await Inventory.findOne({
-      brand, model, variant, color
-    });
-
-    if (inventory) {
-      inventory.quantity += Number(quantity);
-      if (price) inventory.price = price;
-      await inventory.save();
-
-      return res.status(200).json({
-        message: "Stock updated",
-        stock: inventory
-      });
+    if (!req.file) {
+      return res.status(400).json({ message: "Excel file required" });
     }
 
-  const newStock = await Inventory.create({
-  chachesNumber,
-  brand,
-  model,
-  variant,
-  color,
-  quantity: Number(quantity),
-  price,
-  status: quantity > 0 ? "In Stock" : "Out of Stock"
-});
+    const workbook = xlsx.read(req.file.buffer, { type: "buffer" });
+    const sheetName = workbook.SheetNames[0];
+    const sheetData = xlsx.utils.sheet_to_json(
+      workbook.Sheets[sheetName]
+    );
 
-   res.status(201).json(newStock);
+    for (const item of sheetData) {
+      const {
+        chachesNumber,
+        brand,
+        model,
+        variant,
+        color,
+        quantity,
+        price
+      } = item;
+
+      if (!chachesNumber || !model || !variant || !color || !price) continue;
+
+      const inventory = await Inventory.findOne({ chachesNumber });
+
+      if (inventory) {
+        inventory.quantity += Number(quantity || 0);
+        inventory.price = price;
+        inventory.status =
+          inventory.quantity > 0 ? "In Stock" : "Out of Stock";
+        await inventory.save();
+      } else {
+        await Inventory.create({
+          chachesNumber,
+          brand,
+          model,
+          variant,
+          color,
+          quantity: Number(quantity),
+          price,
+          status: quantity > 0 ? "In Stock" : "Out of Stock"
+        });
+      }
+    }
+
+    res.json({
+      message: "Excel inventory uploaded successfully"
+    });
 
   } catch (error) {
     console.error(error);
     res.status(500).json({
-      message: "Inventory error",
+      message: "Excel upload failed",
       error: error.message
     });
   }
 });
+
 
 
 // Get ALL Inventory (Excel type list)
